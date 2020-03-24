13a0b5e0edf9d9e4010ed51a662226cf7c8146be
igor marinho <igormarinholeo2014@gmail.com>
2020-03-24 06:33:38 -0300

    basic start

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+require('./src/index')(app)
+
+app.listen(9000, () => {
+    console.log('Express Up')
+})
